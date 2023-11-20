79ad6515ba815f7b113c4e86db90096bfaeb9c9e
Danil <danilzign0909@gmail.com>
2023-11-20 16:23:02 +0300

    created guess number game

---
+++ guessNumber.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+)
+
+func Guess() {
+	min := 0
+	max := 1000
+
+	var comparison string
+
+	for i := 0; i <= 10; i++ {
+		hiddenNumber := (max + min) / 2
+		fmt.Println("Твое число", hiddenNumber, "?")
+		fmt.Scan(&comparison)
+
+		switch comparison {
+		case "<":
+			max = hiddenNumber
+		case ">":
+			min = hiddenNumber
+		case "=":
+			fmt.Println("Твое число:", hiddenNumber)
+			i = 10
+		default:
+			fmt.Println("Неправильный ввод")
+			i = 9
+		}
+
+		if hiddenNumber > 1000 || hiddenNumber < 0 {
+			break
+		}
+
+	}
+}
